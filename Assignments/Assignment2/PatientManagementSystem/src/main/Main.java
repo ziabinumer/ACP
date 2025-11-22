dd25821cd9a78d0c7ebc8907178c8a97d941e9f3
Zia <ziabinumer@gmail.com>
2025-11-22 15:43:08 +0500

    add db initialization

---
+++ Assignments/Assignment2/PatientManagementSystem/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import database.DatabaseManager;
+
+class Main {
+    public static void main(String[] args) {
+        // initiliaze the db
+        DatabaseManager.start();
+
+        
+    }
+}
