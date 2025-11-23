c15cad04bfb6cb5f5164691e3d26f6db388547c9
Zia <ziabinumer@gmail.com>
2025-11-23 22:35:39 +0500

    testing for disease

--- Assignments/Assignment2/PatientManagementSystem/src/main/TestDAO.java
+++ Assignments/Assignment2/PatientManagementSystem/src/main/TestDAO.java
@@ -12,6 +12,13 @@ public class TestDAO {
 
         int result = dao.add(disease);
         System.out.println("Insert returned: " + result);
+        System.out.println("There are " + dao.getAll().size() + " diseases");
+        dao.delete(1);
+        System.out.println("There are " + dao.getAll().size() + " diseases after delete");
+
+        Disease ds1 = dao.findById(2);
+        ds1.setName("Covid");
+        dao.update(ds1);
 
         DoctorDAOImpl dao2 = new DoctorDAOImpl();
         Doctor d1 = new Doctor("Ali", 1);
