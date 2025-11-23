f5e7113bd3641df092ed22070a3736680ec625c3
Zia <ziabinumer@gmail.com>
2025-11-23 19:40:18 +0500

    structure

--- Assignments/Assignment2/PatientManagementSystem/src/main/Main.java
+++ Assignments/Assignment2/PatientManagementSystem/src/main/Main.java
@@ -2,11 +2,12 @@ package main;
 
 import database.DatabaseManager;
 
+
 class Main {
     public static void main(String[] args) {
         // initiliaze the db
         DatabaseManager.start();
-
+       
         
     }
 }
