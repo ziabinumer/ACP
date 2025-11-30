dcc9eaa25f2b8436c23603ddc30fdcbb3f7bf913
Zia Gondal <89640018+ziabinumer@users.noreply.github.com>
2025-11-30 21:00:40 +0500

    initial testing setup

--- Assignments/Assignment2/PatientManagementSystem/src/gui/AdminMainFrame.java
+++ Assignments/Assignment2/PatientManagementSystem/src/gui/AdminMainFrame.java
@@ -1,20 +1,48 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.GridLayout;
+import javax.swing.border.Border;
 
-public class AdminMainFrame {
-    JFrame frame;
+import java.awt.*;
+
+public class AdminMainFrame extends Layout {
     public AdminMainFrame() {
-        frame = new JFrame("Admin Dashboard");
-        frame.setTitle("Patient Management System: Login");
-        frame.setSize(400, 350);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        super("Patient Management System: Admin");
+        setSize(1000, 600);
+        setVisible(true);
+    }
+
+    @Override
+    protected JPanel createMenu() {
+        /*
+            Add Patient - Add Doctor
+        */
+        JMenuBar menuBar = new JMenuBar();
+         
+        JMenu menu = new JMenu("Add");
+
+        JMenuItem addPatient = new JMenuItem("Add Patient");
+        JMenuItem addDoctor = new JMenuItem("Add Patient");
+        JMenuItem addDisease = new JMenuItem("Add Patient");
+
+        menu.add(addPatient);
+        menu.add(addDoctor);
+        menu.add(addDisease);
+
+        menuBar.add(menu);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(menuBar, BorderLayout.CENTER);
+        return panel;
+    }
+
+    @Override
+    protected void initializeContent() {
+        JLabel welcomeLabel = new JLabel("Welcome");
+        contentArea.add(welcomeLabel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
-        new AdminMainFrame();
+        SwingUtilities.invokeLater(() -> new AdminMainFrame());
     }
 }
