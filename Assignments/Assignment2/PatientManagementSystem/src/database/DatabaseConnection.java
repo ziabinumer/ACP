1ca2303fce56a6277fc04dabc54f849367fa1354
Zia Gondal <89640018+ziabinumer@users.noreply.github.com>
2025-12-16 01:24:31 +0500

    remove static block

--- Assignments/Assignment2/PatientManagementSystem/src/database/DatabaseConnection.java
+++ Assignments/Assignment2/PatientManagementSystem/src/database/DatabaseConnection.java
@@ -7,14 +7,15 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:sqlite:resources/config/hospital.db";
 
-    static {
-        try {
-            Class.forName("org.sqlite.JDBC"); // load driver
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    // static {
+    //     try {
+    //         Class.forName("org.sqlite.JDBC"); // load driver
+    //     } catch (ClassNotFoundException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
+    // modern method. service provider auto loads the driver
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
